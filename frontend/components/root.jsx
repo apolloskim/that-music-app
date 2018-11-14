e43aca591960eac3c5cdfb72ef0fe82e472c8651
Apollos <apolloskim94@gmail.com>
2018-11-14 15:20:19 -0500

    Design side nav bar for that music app

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,6 +4,7 @@ import {HashRouter, Route} from 'react-router-dom';
 import App from './App';
 import SignupFormContainer from './signup_form_container';
 import LoginFormContainer from './login_form_container';
+import Navbar from './navbar/navbar';
 
 const Root = ({store}) => {
   return (
@@ -13,6 +14,7 @@ const Root = ({store}) => {
           <Route exact path="/" component={App} />
           <Route exact path='/signup' component={SignupFormContainer} />
           <Route exact path='/login' component={LoginFormContainer} />
+          <Route exact path='/browse' component={Navbar} />
         </div>
       </HashRouter>
     </Provider>

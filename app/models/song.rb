c7e78edf9e1669fe252abb7e641615860330afa3
Apollos <apolloskim94@gmail.com>
2018-11-15 14:46:34 -0500

    Add image and song attachment to song object

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,8 @@ class Song < ApplicationRecord
   validates :title, presence: true
 
   has_one_attached :song_file
+  has_one_attached :image
 end
 
-# s.song_file.attach(io: File.open("/Users/apollos/Downloads/Drake-Scorpion-2018/Drake-Scorpion/2-08-Blue-Tint.mp3"), filename: "2-08-Blue-Tint.mp3")
+# s.song_file.attach(io: File.open("/Users/apollos/Downloads/Drake-Scorpion-2018/Drake-Scorpion/1-05 God's Plan.mp3"), filename: "1-05 God's Plan.mp3")
+# s.image.attach(io: File.open("/Users/apollos/Downloads/drake-scorpion-cover-art.jpg"), filename: "drake-scorpion-cover-art.jpg")

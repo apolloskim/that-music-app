d80544c86fff2afe0578296364969e365d149006
Apollos <apolloskim94@gmail.com>
2018-11-14 23:31:47 -0500

    Create songs table

---
+++ db/migrate/20181115042826_create_songs.rb
@@ -0,0 +1,9 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title, null: false
+      t.integer :album_id, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_042649) do
+ActiveRecord::Schema.define(version: 2018_11_15_042826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2018_11_15_042649) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

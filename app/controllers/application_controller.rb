9730c5258e6c91aaa7e30a515374a8118a17db39
Apollos <apolloskim94@gmail.com>
2018-11-26 22:40:35 -0500

    Change default song

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_song(user)
-    Song.find(user.current_song_id || 300)
+    Song.find(user.current_song_id || 320)
   end
 
   def require_logged_in

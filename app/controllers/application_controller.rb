397b496a33df2132ab5c1466ee773b7817b20b23
Apollos <apolloskim94@gmail.com>
2018-11-26 22:54:36 -0500

    default song id = 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_song(user)
-    Song.find(user.current_song_id || 200)
+    Song.find(user.current_song_id || 1)
   end
 
   def require_logged_in

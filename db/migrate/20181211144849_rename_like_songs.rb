e659b4a0e641c9374cf562879c1886de4da5e6cc
Apollos <apolloskim94@gmail.com>
2019-02-08 16:57:10 -0500

    add end in rename_like_songs migration file

--- db/migrate/20181211144849_rename_like_songs.rb
+++ db/migrate/20181211144849_rename_like_songs.rb
@@ -1,7 +1,8 @@
 class RenameLikeSongs < ActiveRecord::Migration[5.2]
   def change
     rename_table :like_songs do |t|
-    t.integer :user_id
-    t.integer :album_id
+      t.integer :user_id
+      t.integer :album_id
+    end
   end
 end

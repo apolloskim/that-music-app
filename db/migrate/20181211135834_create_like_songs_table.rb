977c6a3d5877e2d7e04dc7604abfb1bb97605946
Apollos <apolloskim94@gmail.com>
2019-02-08 19:41:07 -0500

    Rename like_songs to likesongs

--- db/migrate/20181211135834_create_like_songs_table.rb
+++ db/migrate/20181211135834_create_like_songs_table.rb
@@ -1,6 +1,6 @@
 class CreateLikeSongsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :like_songs do |t|
+    create_table :likesongs do |t|
       t.integer :user_id
       t.integer :song_id
     end
